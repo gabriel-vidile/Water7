65b1728f0530a665a135079d56ef57ec2e6b5485
Gabriel Vidile <gabrielvidile@gmail.com>
2024-07-10 21:31:19 -0300

    Adicionando dados da api de rain

--- main.go
+++ main.go
@@ -3,13 +3,13 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"sync"
 )
 
 var (
-	rainData       float64
 	irrigationData []IrrigationData
 	dataLock       sync.Mutex
 )
@@ -24,15 +24,56 @@ type IrrigationData struct {
 	SirenSound         string  `json:"sirenSound"`
 }
 
-// Função para buscar dados de chuva
+// Estrutura para os dados de resposta da API pública
+type WeatherResponse struct {
+	Temp     float64 `json:"temp"`
+	Date     string  `json:"date"`
+	Rain     float64 `json:"rain"`
+	CityName string  `json:"city_name"`
+	Forecast []struct {
+		Date string  `json:"date"`
+		Max  float64 `json:"max"`
+		Min  float64 `json:"min"`
+		Rain float64 `json:"rain"`
+	} `json:"forecast"`
+}
+
 func getRainDataHandler(w http.ResponseWriter, r *http.Request) {
+	apiKey := "12eef7aa"
+	url := fmt.Sprintf("https://api.hgbrasil.com/weather?woeid=440202&array_limit=2&fields=only_results,temp,city_name,forecast,max,min,date,rain&key=%s", apiKey)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		http.Error(w, "Erro ao buscar dados de clima", http.StatusInternalServerError)
+		return
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		http.Error(w, "Erro ao ler resposta da API", http.StatusInternalServerError)
+		return
+	}
+
+	log.Printf("Resposta da API: %s", body)
+
+	var weatherResponse WeatherResponse
+	err = json.Unmarshal(body, &weatherResponse)
+	if err != nil {
+		http.Error(w, "Erro ao fazer parse da resposta da API", http.StatusInternalServerError)
+		return
+	}
+
+	// Supondo que estamos interessados na previsão de hoje
+	rainData := weatherResponse.Forecast[0].Rain
+	log.Printf("Dados de Chuva: %f", rainData)
+
 	dataLock.Lock()
 	defer dataLock.Unlock()
 	response := map[string]float64{"rainData": rainData}
 	json.NewEncoder(w).Encode(response)
 }
 
-// Função para guardar dados de irrigação
 func saveIrrigationDataHandler(w http.ResponseWriter, r *http.Request) {
 	var data IrrigationData
 	err := json.NewDecoder(r.Body).Decode(&data)
